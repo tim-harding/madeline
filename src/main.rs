b6d78dde709c83a911132b67a7fb5811ccb31bf7
Tim Harding <tim@timharding.co>
2023-09-12 22:10:00 -0700

    Removed workspaces again

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::fmt::{self, Display, Formatter};
-
-pub use wgpu::SurfaceError;
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
 
 #[derive(Debug, Default, Clone, Copy)]
 pub struct Vec2<T> {
@@ -97,7 +99,7 @@ impl State {
         }
     }
 
-    pub fn render(&mut self) -> Result<(), SurfaceError> {
+    pub fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
         let output = self.surface.get_current_texture()?;
         let view = output
             .texture
@@ -131,4 +133,61 @@ impl State {
         output.present();
         Ok(())
     }
+}
+
+pub fn main() {
+    pollster::block_on(main_async());
+}
+
+async fn main_async() {
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let size = window.inner_size();
+    let mut state = State::new(&window, Vec2::new(size.width, size.height)).await;
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => match event {
+            WindowEvent::CloseRequested
+            | WindowEvent::KeyboardInput {
+                input:
+                    KeyboardInput {
+                        state: ElementState::Pressed,
+                        virtual_keycode: Some(VirtualKeyCode::Escape),
+                        ..
+                    },
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            WindowEvent::Resized(size) => state.resize(Vec2::new(size.width, size.height)),
+            WindowEvent::ScaleFactorChanged {
+                scale_factor,
+                new_inner_size,
+            } => {
+                state.resize(Vec2::new(new_inner_size.width, new_inner_size.height));
+                state.scale = *scale_factor;
+            }
+            _ => {}
+        },
+
+        Event::RedrawRequested(window_id) if window_id == window.id() => {
+            match state.render() {
+                Ok(_) => {}
+                // Reconfigure the surface if lost
+                Err(wgpu::SurfaceError::Lost) => state.resize(state.size()),
+                // The system is out of memory, we should probably quit
+                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                // All other errors (Outdated, Timeout) should be resolved by the next frame
+                Err(e) => eprintln!("{:?}", e),
+            }
+        }
+
+        Event::MainEventsCleared => {
+            // RedrawRequested will only trigger once, unless we manually
+            // request it.
+            window.request_redraw();
+        }
+        _ => {}
+    });
 }
