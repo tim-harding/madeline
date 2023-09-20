8023f2e9dc94409ff53318347f0912eb15368d5f
Tim Harding <tim@timharding.co>
2023-09-20 13:36:59 -0700

    Compiling example program

--- src/main.rs
+++ src/main.rs
@@ -1,358 +1,94 @@
-use std::ops::{Add, Sub};
-
-use wgpu::util::DeviceExt;
-use winit::{
-    event::*,
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
-
-#[derive(Debug, Default, Clone, Copy)]
-pub struct Vec2<T> {
-    pub x: T,
-    pub y: T,
-}
-
-impl<T> Add<Vec2<T>> for Vec2<T>
-where
-    T: Add<Output = T>,
-{
-    type Output = Self;
-
-    fn add(self, rhs: Vec2<T>) -> Self::Output {
-        Self::new(self.x + rhs.x, self.y + rhs.y)
-    }
-}
-
-impl<T> Sub<Vec2<T>> for Vec2<T>
-where
-    T: Sub<Output = T>,
-{
-    type Output = Self;
-
-    fn sub(self, rhs: Vec2<T>) -> Self::Output {
-        Self::new(self.x - rhs.x, self.y - rhs.y)
-    }
-}
-
-impl<T> Vec2<T> {
-    pub fn new(x: T, y: T) -> Self {
-        Self { x, y }
-    }
-}
-
-pub struct State {
-    surface: wgpu::Surface,
-    device: wgpu::Device,
-    queue: wgpu::Queue,
-    config: wgpu::SurfaceConfiguration,
-    render_pipeline: wgpu::RenderPipeline,
-    vertex_buffer: wgpu::Buffer,
-    index_buffer: wgpu::Buffer,
-    size: Vec2<u32>,
-    pub scale: f64,
-}
-
-impl State {
-    pub async fn new<W>(window: &W, size: Vec2<u32>) -> Self
-    where
-        W: raw_window_handle::HasRawWindowHandle + raw_window_handle::HasRawDisplayHandle,
-    {
-        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
-            backends: wgpu::Backends::all(),
-            dx12_shader_compiler: Default::default(),
-        });
-
-        let surface = unsafe { instance.create_surface(&window) }.unwrap();
-
-        let adapter = instance
-            .request_adapter(&wgpu::RequestAdapterOptions {
-                power_preference: wgpu::PowerPreference::HighPerformance,
-                compatible_surface: Some(&surface),
-                force_fallback_adapter: false,
-            })
-            .await
-            .unwrap();
-
-        let (device, queue) = adapter
-            .request_device(
-                &wgpu::DeviceDescriptor {
-                    features: wgpu::Features::empty(),
-                    limits: adapter.limits(),
-                    label: None,
-                },
-                None, // Trace path
-            )
-            .await
-            .unwrap();
-
-        let surface_caps = surface.get_capabilities(&adapter);
-        let surface_format = surface_caps
-            .formats
-            .iter()
-            .copied()
-            .find(|f| f.is_srgb())
-            .unwrap_or(surface_caps.formats[0]);
-        let config = wgpu::SurfaceConfiguration {
-            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-            format: surface_format,
-            width: size.x,
-            height: size.y,
-            present_mode: surface_caps.present_modes[0],
-            alpha_mode: surface_caps.alpha_modes[0],
-            view_formats: vec![],
-        };
-        surface.configure(&device, &config);
-
-        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
-            label: Some("Shader"),
-            source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
-        });
-
-        let render_pipeline_layout =
-            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-                label: Some("Render Pipeline Layout"),
-                bind_group_layouts: &[],
-                push_constant_ranges: &[],
-            });
-
-        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-            label: Some("Render Pipeline"),
-            layout: Some(&render_pipeline_layout),
-            vertex: wgpu::VertexState {
-                module: &shader,
-                entry_point: "vs_main",
-                buffers: &[Vertex::desc()],
-            },
-            fragment: Some(wgpu::FragmentState {
-                module: &shader,
-                entry_point: "fs_main",
-                targets: &[Some(wgpu::ColorTargetState {
-                    format: config.format,
-                    blend: Some(wgpu::BlendState::REPLACE),
-                    write_mask: wgpu::ColorWrites::ALL,
-                })],
-            }),
-            primitive: wgpu::PrimitiveState {
-                topology: wgpu::PrimitiveTopology::TriangleList,
-                strip_index_format: None,
-                front_face: wgpu::FrontFace::Ccw,
-                cull_mode: Some(wgpu::Face::Back),
-                polygon_mode: wgpu::PolygonMode::Fill,
-                unclipped_depth: false,
-                conservative: false,
-            },
-            depth_stencil: None,
-            multisample: wgpu::MultisampleState {
-                count: 1,
-                mask: !0,
-                alpha_to_coverage_enabled: false,
-            },
-            multiview: None,
-        });
-
-        let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("Vertex Buffer"),
-            contents: bytemuck::cast_slice(VERTICES),
-            usage: wgpu::BufferUsages::VERTEX,
-        });
-        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("Index Buffer"),
-            contents: bytemuck::cast_slice(INDICES),
-            usage: wgpu::BufferUsages::INDEX,
-        });
-
-        Self {
-            surface,
-            device,
-            queue,
-            config,
-            render_pipeline,
-            vertex_buffer,
-            index_buffer,
-            size,
-            scale: 1.0,
-        }
-    }
-
-    pub fn size(&self) -> Vec2<u32> {
-        self.size
-    }
-
-    pub fn resize(&mut self, size: Vec2<u32>) {
-        if size.x > 0 && size.y > 0 {
-            self.size = size;
-            self.config.width = size.x;
-            self.config.height = size.y;
-            self.surface.configure(&self.device, &self.config);
-        }
-    }
-
-    pub fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
-        let output = self.surface.get_current_texture()?;
-        let output_view = output
-            .texture
-            .create_view(&wgpu::TextureViewDescriptor::default());
-        let mut encoder = self
-            .device
-            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
-                label: Some("Render Encoder"),
-            });
-
-        {
-            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                label: Some("Render Pass"),
-                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                    view: &output_view,
-                    resolve_target: None,
-                    ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(wgpu::Color {
-                            r: 0.1,
-                            g: 0.2,
-                            b: 0.3,
-                            a: 1.0,
-                        }),
-                        store: true,
-                    },
-                })],
-                depth_stencil_attachment: None,
-            });
-            render_pass.set_pipeline(&self.render_pipeline);
-            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
-            render_pass.draw_indexed(0..INDICES.len() as u32, 0, 0..1);
-        }
-
-        self.queue.submit(std::iter::once(encoder.finish()));
-        output.present();
-        Ok(())
-    }
-}
+use cranelift::codegen::entity::EntityRef;
+use cranelift::codegen::ir::types::*;
+use cranelift::codegen::ir::{AbiParam, Function, InstBuilder, Signature, UserFuncName};
+use cranelift::codegen::isa::CallConv;
+use cranelift::codegen::settings;
+use cranelift::codegen::verifier::verify_function;
+use cranelift::frontend::{FunctionBuilder, FunctionBuilderContext, Variable};
 
 pub fn main() {
-    pollster::block_on(main_async());
-}
+    let mut sig = Signature::new(CallConv::SystemV);
+    sig.returns.push(AbiParam::new(I32));
+    sig.params.push(AbiParam::new(I32));
+    let mut fn_builder_ctx = FunctionBuilderContext::new();
+    let mut func = Function::with_name_signature(UserFuncName::user(0, 0), sig);
+    {
+        let mut builder = FunctionBuilder::new(&mut func, &mut fn_builder_ctx);
 
-async fn main_async() {
-    env_logger::init();
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
-    let size = window.inner_size();
-    let mut state = State::new(&window, Vec2::new(size.width, size.height)).await;
-    event_loop.run(move |event, _, control_flow| match event {
-        Event::WindowEvent {
-            ref event,
-            window_id,
-        } if window_id == window.id() => match event {
-            WindowEvent::CloseRequested
-            | WindowEvent::KeyboardInput {
-                input:
-                    KeyboardInput {
-                        state: ElementState::Pressed,
-                        virtual_keycode: Some(VirtualKeyCode::Escape),
-                        ..
-                    },
-                ..
-            } => *control_flow = ControlFlow::Exit,
-            WindowEvent::Resized(size) => state.resize(Vec2::new(size.width, size.height)),
-            WindowEvent::ScaleFactorChanged {
-                scale_factor,
-                new_inner_size,
-            } => {
-                state.resize(Vec2::new(new_inner_size.width, new_inner_size.height));
-                state.scale = *scale_factor;
-            }
-            _ => {}
-        },
+        let block0 = builder.create_block();
+        let block1 = builder.create_block();
+        let block2 = builder.create_block();
+        let block3 = builder.create_block();
+        let x = Variable::new(0);
+        let y = Variable::new(1);
+        let z = Variable::new(2);
+        builder.declare_var(x, I32);
+        builder.declare_var(y, I32);
+        builder.declare_var(z, I32);
+        builder.append_block_params_for_function_params(block0);
 
-        Event::RedrawRequested(window_id) if window_id == window.id() => {
-            match state.render() {
-                Ok(_) => {}
-                // Reconfigure the surface if lost
-                Err(wgpu::SurfaceError::Lost) => state.resize(state.size()),
-                // The system is out of memory, we should probably quit
-                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
-                // All other errors (Outdated, Timeout) should be resolved by the next frame
-                Err(e) => eprintln!("{:?}", e),
-            }
+        builder.switch_to_block(block0);
+        builder.seal_block(block0);
+        {
+            let tmp = builder.block_params(block0)[0]; // the first function parameter
+            builder.def_var(x, tmp);
+        }
+        {
+            let tmp = builder.ins().iconst(I32, 2);
+            builder.def_var(y, tmp);
+        }
+        {
+            let arg1 = builder.use_var(x);
+            let arg2 = builder.use_var(y);
+            let tmp = builder.ins().iadd(arg1, arg2);
+            builder.def_var(z, tmp);
+        }
+        builder.ins().jump(block1, &[]);
+
+        builder.switch_to_block(block1);
+        {
+            let arg1 = builder.use_var(y);
+            let arg2 = builder.use_var(z);
+            let tmp = builder.ins().iadd(arg1, arg2);
+            builder.def_var(z, tmp);
+        }
+        {
+            let arg = builder.use_var(y);
+            builder.ins().brif(arg, block3, &[], block2, &[]);
         }
 
-        Event::MainEventsCleared => {
-            // RedrawRequested will only trigger once, unless we manually
-            // request it.
-            window.request_redraw();
+        builder.switch_to_block(block2);
+        builder.seal_block(block2);
+        {
+            let arg1 = builder.use_var(z);
+            let arg2 = builder.use_var(x);
+            let tmp = builder.ins().isub(arg1, arg2);
+            builder.def_var(z, tmp);
         }
-        _ => {}
-    });
-}
-
-#[repr(C)]
-#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
-struct Vertex {
-    position: [f32; 3],
-    color: [f32; 3],
-}
-
-impl Vertex {
-    const ATTRIBS: [wgpu::VertexAttribute; 2] =
-        wgpu::vertex_attr_array![0 => Float32x3, 1 => Float32x3];
-
-    fn desc() -> wgpu::VertexBufferLayout<'static> {
-        use std::mem;
-
-        wgpu::VertexBufferLayout {
-            array_stride: mem::size_of::<Self>() as wgpu::BufferAddress,
-            step_mode: wgpu::VertexStepMode::Vertex,
-            attributes: &Self::ATTRIBS,
+        {
+            let arg = builder.use_var(y);
+            builder.ins().return_(&[arg]);
         }
+
+        builder.switch_to_block(block3);
+        builder.seal_block(block3);
+
+        {
+            let arg1 = builder.use_var(y);
+            let arg2 = builder.use_var(x);
+            let tmp = builder.ins().isub(arg1, arg2);
+            builder.def_var(y, tmp);
+        }
+        builder.ins().jump(block1, &[]);
+        builder.seal_block(block1);
+
+        builder.finalize();
+    }
+
+    let flags = settings::Flags::new(settings::builder());
+    let res = verify_function(&func, &flags);
+    println!("{}", func.display());
+    if let Err(errors) = res {
+        panic!("{}", errors);
     }
 }
-
-#[derive(Debug, Default, Clone, PartialEq, Eq)]
-struct Draw {
-    vertices: Vec<Vertex>,
-    indices: Vec<u16>,
-}
-
-impl Draw {
-    pub fn new() -> Self {
-        Self::default()
-    }
-
-    pub fn clear(&mut self) {
-        self.vertices.clear();
-        self.indices.clear();
-    }
-
-    pub fn draw_rectangle(&mut self, position: Vec2<f32>, size: Vec2<f32>) {
-        let base_vertex = self.vertices.len();
-        self.vertices.push(Vertex {
-            position: 
-        })
-    }
-}
-
-const VERTICES: &[Vertex] = &[
-    Vertex {
-        position: [-0.0868241, 0.49240386, 0.0],
-        color: [0.5, 0.0, 0.5],
-    }, // A
-    Vertex {
-        position: [-0.49513406, 0.06958647, 0.0],
-        color: [0.5, 0.0, 0.5],
-    }, // B
-    Vertex {
-        position: [-0.21918549, -0.44939706, 0.0],
-        color: [0.5, 0.0, 0.5],
-    }, // C
-    Vertex {
-        position: [0.35966998, -0.3473291, 0.0],
-        color: [0.5, 0.0, 0.5],
-    }, // D
-    Vertex {
-        position: [0.44147372, 0.2347359, 0.0],
-        color: [0.5, 0.0, 0.5],
-    }, // E
-];
-
-const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
