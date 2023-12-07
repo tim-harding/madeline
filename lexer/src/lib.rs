6c4c9da32285401fc36454951e28d06aa4f9525e
Tim Harding <tim@timharding.co>
2023-12-07 14:39:18 -0800

    Parsing identifiers

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -1,4 +1,8 @@
-use std::{collections::HashMap, iter::Peekable, str::CharIndices};
+use std::{
+    collections::HashMap,
+    iter::Peekable,
+    str::{from_utf8_unchecked, CharIndices},
+};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Token {
@@ -8,21 +12,20 @@ pub struct Token {
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum TokenKind {
-    Ident,
-    Fn,
+    Ident(Id),
     ParenLeft,
     ParenRight,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct IdentifierId(pub u32);
+pub struct Id(pub u32);
 
 pub struct Lexer<'a> {
     line: u32,
     column: u32,
     s: &'a str,
     iter: Peekable<CharIndices<'a>>,
-    identifiers: HashMap<String, IdentifierId>,
+    identifiers: HashMap<&'a str, Id>,
 }
 
 impl<'a> Lexer<'a> {
@@ -31,29 +34,50 @@ impl<'a> Lexer<'a> {
             iter: s.char_indices().peekable(),
             s,
             identifiers: HashMap::new(),
-            line: 0,
-            column: 0,
+            line: 1,
+            column: 1,
         }
     }
 
     pub fn token(&mut self) -> Result<Option<Token>, Error> {
         self.take_whitespace();
-        let Some(next) = self.take() else {
+        let Some((start, c)) = self.take() else {
             return Ok(None);
         };
-        let kind = match next.1 {
-            '(' => Ok(TokenKind::ParenLeft),
-            ')' => Ok(TokenKind::ParenRight),
-            c => Err(Error {
-                line: self.line,
-                column: self.column,
-                c,
-            }),
-        }?;
+        let kind = match c {
+            '(' => TokenKind::ParenLeft,
+            ')' => TokenKind::ParenRight,
+            '_' | 'a'..='z' | 'A'..='Z' => self.word(start),
+            c => {
+                return Err(Error {
+                    line: self.line,
+                    column: self.column,
+                    c,
+                })
+            }
+        };
         Ok(Some(Token {
-            start: next.0.try_into().unwrap(),
+            start: c.try_into().unwrap(),
             kind,
         }))
+    }
+
+    fn word(&mut self, start: usize) -> TokenKind {
+        loop {
+            let s = self.s.as_bytes();
+            let s = match self.peek() {
+                Some((_, '_' | 'a'..='z' | 'A'..='Z')) => {
+                    self.take();
+                    continue;
+                }
+                Some((end, _)) => &s[start..end],
+                None => &s[start..],
+            };
+            let s = unsafe { from_utf8_unchecked(s) };
+            let ident = Id(self.identifiers.len().try_into().unwrap());
+            let ident = *self.identifiers.entry(s).or_insert(ident);
+            break TokenKind::Ident(ident);
+        }
     }
 
     fn take(&mut self) -> Option<(usize, char)> {
@@ -71,7 +95,7 @@ impl<'a> Lexer<'a> {
                 Some((_, '\n')) => {
                     self.take();
                     self.line += 1;
-                    self.column = 0;
+                    self.column = 1;
                 }
                 Some((_, c)) if c.is_whitespace() => {
                     self.take();
@@ -80,6 +104,18 @@ impl<'a> Lexer<'a> {
             }
         }
     }
+}
+
+/// Gets the line and column number of the token at the given byte offset.
+///
+/// SAFETY: token_start must index the start of a UTF-8 char
+pub unsafe fn token_line_and_column(s: &str, token_start: usize) -> (u32, u32) {
+    let s = &s.as_bytes()[..token_start];
+    let s = unsafe { from_utf8_unchecked(s) };
+    s.chars().fold((1, 1), |(line, column), c| match c {
+        '\n' => (line + 1, 1),
+        _ => (line, column + 1),
+    })
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, thiserror::Error)]
@@ -109,7 +145,12 @@ mod tests {
     }
 
     #[test]
-    fn left_paren() {
+    fn parens() {
         assert_tokens_match(" ( ) ", [ParenLeft, ParenRight])
+    }
+
+    #[test]
+    fn idents() {
+        assert_tokens_match(" hi hello ", [Ident(Id(0)), Ident(Id(1))])
     }
 }
